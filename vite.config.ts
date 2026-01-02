4b495022e2724950583e1252cdbc8ece66878d84
Thformony <your-email@example.com>
2026-01-02 04:26:55 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: isGithubPages ? '//' : '/',
+    base: isGithubPages ? '/Bab-Alhara/' : '/',
     resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
   }
 })
